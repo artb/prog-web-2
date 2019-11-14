941da58f32798d20cf76e6f48abcbdbe96acad5f
Arthur Binda <artbinda@gmail.com>
2019-11-14 19:01:59 -0300

    Create de curso já funciona

--- express_tutorial/app/controlers/curso.js
+++ express_tutorial/app/controlers/curso.js
@@ -1,39 +1,28 @@
 var models = require('../models/index');
 var Curso = models.curso;
-const index = (req, res) => {};
-const read = (req, res) => {};
-const update = (req, res) => {};
-const remove = (req, res) => {};
+const read = (req, res) => { };
+const update = (req, res) => { };
+const remove = (req, res) => { };
 
+const index = async (req, res) => {
+    const cursos = await Curso.findAll();
+    res.render('curso/index', {
+        cursos: cursos,
+    });
+};
 
 const create = async (req, res) => {
     if (req.route.methods.get) {
         res.render('curso/create');
     } else {
-        try {
-        await Curso.create(req.body);
-        } catch (e) {
-            res.render('curso/create', {
-                curso: req.body,
-                errors: error.errors
-            });
-        }
+        curso = await Curso.create({
+            sigla: req.body.sigla,
+            nome: req.body.nome,
+            descricao: req.body.descricao,
+            id_area: req.body.area,
+        });
+        res.redirect('/curso');
     }
 };
-   
- 
-   const showError = function (errors, field) {
-    var mensagem;
-    if (typeof errors != 'undefined') {
-        errors.forEach(function (error) {
-            console.log(error);
-            if (error.path == field) {
-                mensagem = error.message;
-                return;
-            }
-    });
-        return mensagem;
-    }
-}    
 
-module.exports = { index, read, create, update, remove, showError}
+module.exports = { index, read, create, update, remove}
--- express_tutorial/config/router.js
+++ express_tutorial/config/router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('../app/controlers/main');
-const areaController = require('../app/controlers/area')
-const cursoController = require('../app/controlers/curso')
+const areaController = require('../app/controlers/area');
+const cursoController = require('../app/controlers/curso');
 
 // MainController
 router.get('/', mainController.index);
@@ -12,12 +12,12 @@ router.get('/sobre', mainController.sobre);
 router.get('/area', areaController.index)
 
 // CursoController
-router.get('/curso' , cursoController.index);
-router.get('/curso/read/:id' , cursoController.read);
-router.get('/curso/create' , cursoController.create);
-router.post('/curso/create' , cursoController.create);
-router.get('/curso/update/:id' , cursoController.update);
-router.post('/curso/update/:id' , cursoController.update);
-router.post('/curso/remove/:id' , cursoController.remove);
+router.get('/curso', cursoController.index);
+router.get('/curso/read/:id', cursoController.read);
+router.get('/curso/create', cursoController.create);
+router.post('/curso/create', cursoController.create);
+router.get('/curso/update/:id', cursoController.update);
+router.post('/curso/update/:id', cursoController.update);
+router.post('/curso/remove/:id', cursoController.remove);
 
 module.exports = router;
